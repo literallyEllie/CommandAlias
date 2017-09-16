3c9186bd2d6f8db10befa420f6fc9c00cc98decf
Ellie <ellieh6712@Gmail.com>
2017-09-16 14:03:43 +0100

    Version 1.1.3 + Updated kotlin version + Custom no permission message + Plugin capability  to send custom messages which don't relate to any particular command. + {prefix} placeholder in config + Unfixed the default config (oops?) + Small code cleanup + Rewrote the command system so its more OOP (and more efficient) + Now loads all commands regardless of the config boolean "enabled" + /ca toggle - actually gives an accurate response message now + Better error handling - it tells you error in game and doesn't break your config <3

---
+++ src/main/kotlin/de/elliepotato/commandalias/backend/CommandType.kt
@@ -0,0 +1,28 @@
+package de.elliepotato.commandalias.backend
+
+/**
+ * Created by Ellie on 16/09/2017 for PublicPlugins.
+ *
+ *    Copyright 2017 Ellie
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+enum class CommandType(val prefix: String) {
+
+    MSG("-msg-"),
+    CMD(""), /* at the bottom so others can get matched first */
+
+    ;
+
+
+}
